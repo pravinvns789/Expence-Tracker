7cc474254f9c85c8bfad393f3cfbd9bf01b1623d
pravinvns789 <pravin.singh_cs19@gla.ac.in>
2022-07-26 17:41:42 +0530

    ksbvu

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import ExpenseTracker from './components/ExpenseTracker';
 
 function App() {
   return (
     <div>
+      <h1>hey</h1>
+      <ExpenseTracker/>
       
     </div>
   );
---
+++ client/src/components/ExpenseTracker.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ExpenseTracker = () => {
+    return (
+        <div>
+            pravin singh
+        </div>
+    );
+}
+
+export default ExpenseTracker;
