a80118709cbedd6b85d703ea31c893a022550d07
pravinvns789 <pravin.singh_cs19@gla.ac.in>
2022-07-29 13:43:43 +0530

    expenses

--- client/src/components/ExpenseDate.js
+++ client/src/components/ExpenseDate.js
@@ -6,10 +6,10 @@ const ExpenseDate = (props) => {
   const month = props.date.toLocaleString('en-US', { month: "long" });
   const year = props.date.getFullYear();
   return (
-    <div>
-      <div>{day}</div>
-      <div>{month}</div>
-      <div>{year}</div>
+    <div className="expense-date">
+      <div className="expense-date__day">{day}</div>
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
     </div>
   );
 };
