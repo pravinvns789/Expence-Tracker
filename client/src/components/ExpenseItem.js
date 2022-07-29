f4c8734ad3899fe91c2d584115c78c0888a2f94a
pravinvns789 <pravin.singh_cs19@gla.ac.in>
2022-07-29 12:37:56 +0530

    date correction

--- client/src/components/ExpenseItem.js
+++ client/src/components/ExpenseItem.js
@@ -2,10 +2,17 @@ import React from "react";
 import "./ExpenseItem.css";
 
 const ExpenseTracker = (props) => {
+  const day = props.date.toLocaleString('en-US', { day: "2-digit" });
+  const month = props.date.toLocaleString('en-US', { month: "long" });
+  const year = props.date.getFullYear();
 
   return (
     <div className="expense-item">
-      <div>{props.date.toString()}</div>
+      <div>
+        <div>{day}</div>
+        <div>{month}</div>
+        <div>{year}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
