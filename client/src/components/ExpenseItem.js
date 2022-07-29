f660fa535a08a967d38e410f943fc6c83f1f77d2
pravinvns789 <pravin.singh_cs19@gla.ac.in>
2022-07-29 12:52:33 +0530

    expense date css

--- client/src/components/ExpenseItem.js
+++ client/src/components/ExpenseItem.js
@@ -1,17 +1,14 @@
 import React from "react";
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
 const ExpenseTracker = (props) => {
-  const day = props.date.toLocaleString('en-US', { day: "2-digit" });
-  const month = props.date.toLocaleString('en-US', { month: "long" });
-  const year = props.date.getFullYear();
+  
 
   return (
     <div className="expense-item">
       <div>
-        <div>{day}</div>
-        <div>{month}</div>
-        <div>{year}</div>
+        <ExpenseDate date={props.date}/>
       </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
