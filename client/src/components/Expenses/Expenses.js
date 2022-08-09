29b6ed4fb6eeb942f7dec6501b768798baaca1fc
pravinvns789 <pravin.singh_cs19@gla.ac.in>
2022-08-09 22:42:23 +0530

    conditional return

--- client/src/components/Expenses/Expenses.js
+++ client/src/components/Expenses/Expenses.js
@@ -1,38 +1,35 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpenseFilter";
 
 const Expenses = (props) => {
-  const [filterYear,setFilter]=useState('2020');
-  const onChooseHandler=(choosenYear)=>{
+  const [filterYear, setFilter] = useState("2022");
+  const onChooseHandler = (choosenYear) => {
     setFilter(choosenYear);
+  };
+  const filteredYear = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filterYear;
+  });
+  let expenseContent = <p>No Expense found</p>;
+  if (filteredYear.length > 0) {
+    expenseContent = filteredYear.map((expense) => {
+      return (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      );
+    });
   }
   return (
     <div>
-      <Card className="expenses"> 
-        <ExpensesFilter defalutYear={filterYear} onChoose={onChooseHandler} />
-        <ExpenseItem
-          title={props.expenses[0].title}
-          amount={props.expenses[0].amount}
-          date={props.expenses[0].date}
-        />
-        <ExpenseItem
-          title={props.expenses[1].title}
-          amount={props.expenses[1].amount}
-          date={props.expenses[1].date}
-        />
-        <ExpenseItem
-          title={props.expenses[2].title}
-          amount={props.expenses[2].amount}
-          date={props.expenses[2].date}
-        />
-        <ExpenseItem
-          title={props.expenses[3].title}
-          amount={props.expenses[3].amount}
-          date={props.expenses[3].date}
-        />
+      <Card className="expenses">
+        <ExpensesFilter defaultYear={filterYear} onChoose={onChooseHandler} />
+        {expenseContent}
       </Card>
     </div>
   );
