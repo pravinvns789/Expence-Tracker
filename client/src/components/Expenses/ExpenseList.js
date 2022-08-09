4f9f07a3bf0c6cbb47dd5e19ec8e61802790bf81
pravinvns789 <pravin.singh_cs19@gla.ac.in>
2022-08-09 22:55:18 +0530

    expenseList

---
+++ client/src/components/Expenses/ExpenseList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ExpenseList.css';
+
+const ExpenseList = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default ExpenseList;
