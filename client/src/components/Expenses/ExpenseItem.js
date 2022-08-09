04e73393f140f0adcafa0901bbf281ea3043a441
pravinvns789 <pravin.singh_cs19@gla.ac.in>
2022-08-09 22:42:35 +0530

    conditional return

--- client/src/components/Expenses/ExpenseItem.js
+++ client/src/components/Expenses/ExpenseItem.js
@@ -1,25 +1,17 @@
-import React,{useState} from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseTracker = (props) => {
-  const [title, setTitle]=useState(props.title);
-  const clickHandler=()=>{
-    setTitle('Updated');
-  };
-  
-
   return (
     <Card className="expense-item">
       <div>
-        <ExpenseDate date={props.date}/>
+        <ExpenseDate date={props.date} />
       </div>
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}> click here</button>
     </Card>
   );
 };
